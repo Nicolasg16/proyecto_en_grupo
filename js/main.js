c88e09f951b35a2b15818054d0ab946108a81a0a
Nicolas Guzman <nicolas149910@gmail.com>
2024-01-17 21:44:37 -0500

    git commit -m 'Avances'

--- js/main.js
+++ js/main.js
@@ -1,2 +1,21 @@
 "use strict";
-console.log('Hola desde typescript');
+const $form = document.querySelector('form'); //Se llama HTML
+$form.addEventListener('submit', generarGrupos); // Escucha el evento submit
+const grupos = [];
+function generarGrupos(evento) {
+    evento.preventDefault();
+    const grupo = {
+        nombreIntegrantes: $form.nombreIntegrantes.value,
+        numeroIntegrantes: $form.numeroIntegrantes.value, //Funcion que retorna lo que almaceno en generarGrupos
+    };
+    grupos.push(grupo);
+    $form.reset();
+    console.log(grupo);
+}
+// function obtenerNumeroGrupos(): number {
+// const cantidadParticipantes = grupos.reduce((total, grupo) => total + grupo.numeroIntegrantes, 0);
+//const cantidadGrupos = grupos.length;
+//const numeroGrupos = cantidadGrupos > 0 ? Math.ceil(cantidadParticipantes / cantidadGrupos) : 0;
+//return numeroGrupos;
+//console.log(numeroGrupos);
+//}
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,35 @@
-console.log('Hola desde typescript')
+const $form = document.querySelector('form') as HTMLFormElement; //Se llama HTML
+$form.addEventListener('submit', generarGrupos); // Escucha el evento submit
+
+interface Grupo {
+  nombreIntegrantes: string;
+  numeroIntegrantes:number; //Se crea la interface del tipo de dato que va a recibir
+}
+
+const grupos: Grupo [] = [];
+
+function generarGrupos(evento: Event): void {
+  evento.preventDefault();
+  const grupo: Grupo = {
+  nombreIntegrantes: $form.nombreIntegrantes.value,
+  numeroIntegrantes: $form.numeroIntegrantes.value, //Funcion que retorna lo que almaceno en generarGrupos
+  };
+  grupos.push(grupo);
+  $form.reset();
+  console.log(grupo);
+  }
+
+ // function obtenerNumeroGrupos(): number {
+   // const cantidadParticipantes = grupos.reduce((total, grupo) => total + grupo.numeroIntegrantes, 0);
+    //const cantidadGrupos = grupos.length;
+  
+    //const numeroGrupos = cantidadGrupos > 0 ? Math.ceil(cantidadParticipantes / cantidadGrupos) : 0;
+  
+ //return numeroGrupos;
+    //console.log(numeroGrupos);
+  //}
+
+  
+  
 
 
